dbbddac634f79bc24fb7df95d9260ca59d4051aa
Ppon-crow <crappin44@yahoo.co.jp>
2025-04-23 10:50:26 +0900

    Initial commit

--- index.js
+++ index.js
@@ -25,9 +25,56 @@ program.command('build').description('xvba build [package] - Build Xvba Package 
 //Add new vba files
 program.command('add [m,c]').description('xvba add -c [sub-folder/file] or xvba add -m [sub-folder/file] - Create Vba file type module bas or class cls').action( async (filePath) => { addVbaFile(filePath,program) })
 
+program.command('export')
+  .description('xvba export - Export VBA modules from Excel file')
+  .action(() => {
+    const fs = require('fs');
+    const path = require('path');
 
+    // config.json を読み込む
+    const configPath = path.join(__dirname, 'config.json');
+    if (!fs.existsSync(configPath)) {
+      console.error('Error: config.json not found. Please run "xvba init" first.');
+      process.exit(1);
+    }
 
-program.command('init').description('xvba init - Create a new config files').action();
+    const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+    const excelFile = config.excel_file;
+
+    if (!excelFile || !fs.existsSync(excelFile)) {
+      console.error(`Error: Excel file "${excelFile}" not found.`);
+      process.exit(1);
+    }
+
+    console.log(`Exporting VBA modules from ${excelFile}...`);
+    // エクスポート処理をここに記述
+    // 例: VBA モジュールをエクスポートして xva_modules フォルダに保存
+  });
+
+program.command('init')
+  .description('xvba init - Create a new config files')
+  .action(() => {
+    console.log('Initializing XVBA...');
+    // 必要な初期化処理をここに記述
+    // 例: config.json ファイルを作成する処理
+    const fs = require('fs');
+    const configContent = {
+      app_name: "XVBA",
+      description: "",
+      author: "",
+      email: "",
+      create_date: new Date().toString(),
+      excel_file: "example.xlsm",
+      ribbon_file: "customUI14",
+      ribbon_folder: "ribbons",
+      logs: "on",
+      xvba_packages: {},
+      xvba_dev_packages: {}
+    };
+
+    fs.writeFileSync('config.json', JSON.stringify(configContent, null, 2));
+    console.log('config.json has been created.');
+  });
 
 program.command('ls').description('xvba ls - List all Packages installed').action(() => { listPackages() });
 
